c1a578691322d4e6c333fd30bf31e3867a26cf5b
rmacnak <rmacnak@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-05-02 17:21:12 +0000

    level editor saves, loads, and resizes without losing work

--- trunk/lvlEditor/Main.java
+++ trunk/lvlEditor/Main.java
@@ -10,15 +10,16 @@ class Main extends JFrame implements ActionListener  {
 		"Water TL", "Water T", "Water TR", "Water R", "Water L", "Water", "Barrel", "Sign", "Fence",
 	"Flower 1", "Flower 2", "Door", "Small House BL", "Small House B Windows", "Small House BR", "Small House ML",
 	"Small House M Window", "Small House M Windows", "Small House MR", "Small House TL","Small House TM", "Small House TR"};
-	JTextField height = new JTextField("40", 10), width = new JTextField("40", 10);
+	JTextField height = new JTextField("40", 3), width = new JTextField("40", 3);
 	JComboBox tile = new JComboBox(supportedTiles);
-	JButton generate = new JButton("generate");
+	JButton save = new JButton("Save");
+	JButton load = new JButton("Load");
 
-	static int lvlWidth = 40;
-	static int lvlHeight = 40;
+	static int lvlWidth = 10;
+	static int lvlHeight = 10;
 	public static final int SQUARESIDE = 16; // each square in the origional pokemon seems to be 16x16
 	static String tileType = supportedTiles[1];
-	Tile tiles[][];
+	Tile tiles[][] = new Tile[10][10];
 	Tile currentTile;
 
 	Main(){
@@ -26,14 +27,18 @@ class Main extends JFrame implements ActionListener  {
 
 
 		JPanel sidePanel = new JPanel();
-		sidePanel.add(height);
+		sidePanel.add(new JLabel("Width:"));
 		sidePanel.add(width);
+		sidePanel.add(new JLabel("Height:"));
+		sidePanel.add(height);
 		sidePanel.add(tile);
-		sidePanel.add(generate);
+		sidePanel.add(save);
+		sidePanel.add(load);
 
 		height.addActionListener(this);
 		width.addActionListener(this);
-		generate.addActionListener(this);
+		save.addActionListener(this);
+		load.addActionListener(this);
 
 		add(new JScrollPane(new DisplayView()));
 		add(sidePanel, BorderLayout.NORTH);
@@ -42,48 +47,98 @@ class Main extends JFrame implements ActionListener  {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(500,500);
 		setVisible(true);
-		setTileValues();
-		currentTile = tiles[0][0];
+		
+		resizeMap(20,20);
 	}
 
+	void resizeMap(int newW, int newH)
+	{
+		Tile[][] newTiles = new Tile[newW][newH];
+		
+		//fill with default tiles
+		for(int x=0; x<newW; x++)
+			for(int y=0; y<newH; y++)
+				newTiles[x][y] = new Tile(x,y);
+				
+		//copy over orginals that will fit
+		for(int x=0; x<newW && x<lvlWidth; x++)
+			for(int y=0; y<newH && y<lvlHeight; y++)
+				if(tiles[x][y]!=null)
+					newTiles[x][y] = tiles[x][y];
+					
+		tiles = newTiles;
+		lvlWidth = newW; width.setText(""+lvlWidth);
+		lvlHeight = newH; height.setText(""+lvlHeight);
+		currentTile = tiles[0][0];
+		repaint();
+	}
+	
 	public void actionPerformed(ActionEvent e)
 	{
 		if (e.getSource() == width)
 		{
-			lvlWidth = new Integer(width.getText());
-			tiles = new Tile[lvlWidth][lvlHeight];
-			setTileValues();
-			currentTile = tiles[0][0];
-			repaint();
+			resizeMap(new Integer(width.getText()), lvlHeight);
 		}
 		else if (e.getSource() == height)
 		{
-			lvlHeight = new Integer(height.getText());
-			tiles = new Tile[lvlWidth][lvlHeight];
-			setTileValues();
-			currentTile = tiles[0][0];
-			repaint();
+			resizeMap(lvlWidth, new Integer(height.getText()));
 		}
-		else if(e.getSource() ==generate)
+		else if(e.getSource() == save)
 		{
 
-
+			String filename = getFile("Save");
+			
 			Node mapNode = new Node("map");
+			mapNode.addSubnode("width",""+lvlWidth);
+			mapNode.addSubnode("height",""+lvlHeight);
 			for(int x = 0; x < lvlWidth; x++)
 				for (int y = 0; y < lvlHeight; y++)
-				{
-					Node tileNode = new Node("tile");
-					tileNode.addSubnode("type", tiles[x][y].toString());
-					tileNode.addSubnode("position", tiles[x][y].getLocation());
-					mapNode.addSubnode(tileNode);
-				}
+					mapNode.addSubnode( tiles[x][y].asNode() );
+					
 			try
 			{
-				OutputStream os = new java.io.FileOutputStream("level.nml");
-				mapNode.tryWriteOn(os); os.flush();os.close();
+				OutputStream os = new FileOutputStream(filename);
+				mapNode.tryWriteOn(os); 
+				os.flush();
+				os.close();
 			}
 			catch (Exception ex) { }
 		}
+		else if(e.getSource() == load)
+		{
+			try{
+				String filename = getFile("Load");
+				Node mapNode = Node.parseFrom(new FileInputStream(filename));
+				int w = new Integer( mapNode.contentOf("width"));
+				int h = new Integer( mapNode.contentOf("height"));
+				resizeMap(w,h);
+				
+				
+				
+				for(Node tileNode: mapNode.subnodes("tile"))
+				{
+					int x = new Integer( tileNode.contentOf("x"));
+					int y = new Integer( tileNode.contentOf("y"));
+					Tile t = new Tile(x,y);
+					t.set(tileNode.contentOf("type"));
+					tiles[x][y] = t;
+				}
+				
+				currentTile = tiles[0][0];
+				repaint();
+				
+			}catch(Exception ex){}
+		}
+	}
+	
+	String getFile(String prompt)
+	{
+		JFileChooser chooser = new JFileChooser("./");
+
+		if(chooser.showDialog(this, prompt ) == JFileChooser.APPROVE_OPTION)
+			return chooser.getSelectedFile().toString();
+			
+		return "";
 	}
 
 	public void setTileValues()
@@ -113,7 +168,7 @@ class Main extends JFrame implements ActionListener  {
 
 			for (int x = 0; x < lvlWidth; x++)
 				for (int y = 0; y < lvlHeight; y++)
-					tiles[x][y].drawImage(this, g);
+					if(tiles[x][y]!=null) tiles[x][y].drawImage(this, g);
 
 		}
 		public Dimension getPreferredSize()
