97a0dca8d6f9ce54e29a94f5f3d0e33a862c5f5c
rmacnak <rmacnak@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-05-06 20:41:37 +0000

    red dot is ash now

--- trunk/game/Player.java
+++ trunk/game/Player.java
@@ -1,11 +1,23 @@
 package game;
 import java.util.*;
+import java.awt.*;
+import javax.swing.*;
 
 //you
 class Player extends Battler {
 	String name;
-	List<Pokemon> party;
-	List<Item> pack;
+	java.util.List<Pokemon> party;
+	java.util.List<Item> pack;
 	Map<Pokemon,Boolean> seenIt;
 	Map<Pokemon,Boolean> caughtIt;
+	
+	ImageIcon ii = new ImageIcon("./entityImages/Player Front.png");
+	Player()
+	{
+		
+	}
+	void drawOn(Graphics2D g){
+	
+		g.drawImage(ii.getImage(),0,0,null);
+	}
 }
