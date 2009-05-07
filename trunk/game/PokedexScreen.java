0c32b2442eb07dd5439cfb7998f3091a06b12c27
asiegle <asiegle@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-05-07 15:22:13 +0000

    PokedexScreen.java stuff

--- trunk/game/PokedexScreen.java
+++ trunk/game/PokedexScreen.java
@@ -1,21 +1,41 @@
+package game;
+
+import java.awt.*;
+import javax.swing.*;
 
 public class PokedexScreen extends Presenter{
 	/*
 	*TODO
-	*make a scrollable JTextArea...etc.
+	*
 	*/
-	public PokedexScreen(){
-		
+	Presenter oldPresenter;
+	
+	final ImageIcon ii = new ImageIcon("./resources/pokedex.png");
+	
+	public PokedexScreen(Presenter oldP){
+		oldPresenter = oldP;		
 	}
 	
 	public void drawOn(Graphics2D g){
-		
+		g.drawImage(ii.getImage(),0,0,null);
+		//trying to find right placement for names
+		int Y = 75;
+		int inc = 40; //increment for y
+		int X = 50;
+		g.setColor(Color.BLACK);
+		g.setFont(new Font("Courier New",Font.BOLD,25));
+		//drawString (str, x, y)
+		g.drawString("AndrewSieg",X,Y);
+		Y += inc;
+		g.drawString("RyanMacnak",X,Y);
 	}
 	
 	public void step(){}
 	
 	public void keyPressed(char key){
-	
+		if (key == 'Q')
+			shell().enterPresenter(oldPresenter);
+			
 	}
 
 }
