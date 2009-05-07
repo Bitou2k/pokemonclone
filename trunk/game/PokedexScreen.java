8925a3dbc6afbeda4bd6eb796c0b9f3f4fdf21cb
asiegle <asiegle@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-05-07 13:46:21 +0000

    added PokedexScreen.java

---
+++ trunk/game/PokedexScreen.java
@@ -0,0 +1,21 @@
+
+public class PokedexScreen extends Presenter{
+	/*
+	*TODO
+	*make a scrollable JTextArea...etc.
+	*/
+	public PokedexScreen(){
+	
+	}
+	
+	public void drawOn(Graphics2D g){
+		
+	}
+	
+	public void step(){}
+	
+	public void keyPressed(char key){
+	
+	}
+
+}
