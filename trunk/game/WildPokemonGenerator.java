9e440823abec1e525516eb45c29d5da321111c3f
rmacnak <rmacnak@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-05-08 01:24:37 +0000

    added WildPokemonGenerators, should be easier for someone to figure out how to finish

--- trunk/game/WildPokemonGenerator.java
+++ trunk/game/WildPokemonGenerator.java
@@ -4,5 +4,25 @@ import java.util.*;
 
 class WildPokemonGenerator {
 
-	Map<PokedexPokemon,Double> pairs; //pokemon prototype->probobility
+	private String name;
+	private double chanceOfAnything;
+	private Map<PokedexPokemon,Double> pairs; //pokemon prototype->probobility
+	
+	private WildPokemonGenerator(Node n)
+	{
+		name = n.contentOf("name");
+		
+		System.out.println("Loaded WildPokemonGenerator: "+name);
+	}
+	
+	private static ArrayList<WildPokemonGenerator> gens = new ArrayList<WildPokemonGenerator>();
+	static { 
+		try{
+			Node root = Node.documentRootFrom("./generators.nml");
+			for(Node n : root.subnodes("generator"))
+				gens.add( new WildPokemonGenerator(n) );
+				
+			System.out.println(gens.size()+" generators!");
+		}catch(Exception e){e.printStackTrace();}
+	}
 }
