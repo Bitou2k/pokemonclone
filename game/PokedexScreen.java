92eb0ed3cec9202b2a9e3d44722509a997d18f44
asiegle <asiegle@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-05-11 15:22:05 +0000

    pokedex

--- game/PokedexScreen.java
+++ game/PokedexScreen.java
@@ -23,6 +23,7 @@ public class PokedexScreen extends Presenter{
 	* cursor index next to the menu
 	*/
 	int menuCursorIndex;
+	int topIndex;
 	/**
 	* true: pokemon list has focus, false: menu has focus
 	*/
@@ -35,9 +36,11 @@ public class PokedexScreen extends Presenter{
 	* contains the strings for the pokedex
 	*/
 	ArrayList<String> pokedexString = new ArrayList<String>();
+	ArrayList<Boolean> caughtList = new ArrayList<Boolean>();
 	
 	
 	final ImageIcon ii = new ImageIcon("./resources/pokedex.png");
+	final ImageIcon pokeball = new ImageIcon("./resources/battle/battlepokeball.png");
 	final ImageIcon arrow = new ImageIcon("./resources/arrow.png");
 	final ImageIcon idleArrow = new ImageIcon("./resources/idlearrow.png");
 	
@@ -47,17 +50,23 @@ public class PokedexScreen extends Presenter{
 		menuCursorIndex = 0;
 		pkmn = true;
 		ash = ((Area)oldP).player;
+		topIndex = 0;
 		
 		//set the pokedex strings
-		java.util.List<PokedexPokemon> pokeList = PokedexPokemon.all();
-		for (PokedexPokemon p : PokedexPokemon.all()){
-			if (ash.seenIt.get(p)){
+		ArrayList<PokedexPokemon> pokeList = PokedexPokemon.all();
+		
+		for (int i = 0; i < pokeList.size(); i++){
+			PokedexPokemon p = pokeList.get(i);
+			System.out.println(p.name());
+			if (ash.seenIt.get(p) == true){
 				pokedexString.add(p.name());
 			}
 			else{
 				pokedexString.add("----------");
 			}
+			caughtList.add((Boolean)ash.caughtIt.get(p));
 		}
+		
 		
 	}
 	
@@ -70,12 +79,11 @@ public class PokedexScreen extends Presenter{
 		g.setColor(Color.BLACK);
 		g.setFont(new Font("Courier New",Font.BOLD,25));
 		//drawString (str, x, y)
-		g.drawString("AndrewSieg",X,Y);
-		Y += inc;
-		g.drawString("RyanMacnak",X,Y);
-		for (int i = 0; i < 4; i++){
+		for (int i = 0; i < 6; i++){			
+			g.drawString(pokedexString.get((pokedexString.size() - 1 - i - topIndex) % pokedexString.size()),X,Y);
+			if (caughtList.get((pokedexString.size() - 1 - i - topIndex) % pokedexString.size()))
+				g.drawImage(pokeball.getImage(),X-20, Y - 15, null);
 			Y += inc;
-			g.drawString("----------",X,Y);
 		}
 		ImageIcon pkmnArrow,menuArrow;
 		if (pkmn){
@@ -86,7 +94,7 @@ public class PokedexScreen extends Presenter{
 			pkmnArrow = idleArrow;
 			menuArrow = arrow;
 		}
-		g.drawImage(pkmnArrow.getImage(), X - 20, 60 + pkmnCursorIndex * inc, null);
+		g.drawImage(pkmnArrow.getImage(), X - 40, 60 + pkmnCursorIndex * inc, null);
 		g.drawImage(menuArrow.getImage(), 240, 158 + menuCursorIndex * 31, null);
 	}
 	
@@ -96,7 +104,8 @@ public class PokedexScreen extends Presenter{
 		if (key == 'Q')	enterPresenter(oldPresenter);
 		else if (key == 'S'){
 			if(pkmn){
-				if (pkmnCursorIndex != 5) pkmnCursorIndex++;
+				if (pkmnCursorIndex != 5){ pkmnCursorIndex++;}
+				else {topIndex++;}
 			}
 			else{
 				if (menuCursorIndex != 5) menuCursorIndex++;
@@ -104,7 +113,8 @@ public class PokedexScreen extends Presenter{
 		}
 		else if (key == 'W'){
 			if(pkmn){
-				if (pkmnCursorIndex != 0) pkmnCursorIndex--;
+				if (pkmnCursorIndex != 0){ pkmnCursorIndex--;}
+				else {topIndex--;}
 			}
 			else{
 				if (menuCursorIndex != 0) menuCursorIndex--;
