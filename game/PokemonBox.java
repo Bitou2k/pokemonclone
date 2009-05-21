85cfef6f26529c3eea6e0029c3bf5e97649d526f
jjftails <jjftails@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-05-21 15:56:24 +0000

    more updates to pokemon box

--- game/PokemonBox.java
+++ game/PokemonBox.java
@@ -20,46 +20,60 @@ public class PokemonBox extends Presenter {
 	*/
 	int pkmnCursorIndex;
 	
+	final ImageIcon bottomBox = new ImageIcon("./resources/bottomBox.png");
 	final ImageIcon arrow = new ImageIcon("./resources/arrow.png");
 	final ImageIcon idleArrow = new ImageIcon("./resources/idlearrow.png");
 
 	public PokemonBox(Presenter oldP){
 		oldPresenter = oldP;		
 		pkmnCursorIndex = 0;
+		
+		//if (player().party()==null)
+		//{
+		//	player().party().add(new Pokemon(Species.named("Ditto"), 5));
+		//}
 	}
 	
 	public void drawOn(Graphics2D g){
 		
+		g.setColor(Color.WHITE);
+		g.fillRect(0,0,320,288);
+		
 		g.setColor(Color.BLACK);
 		g.setFont(new Font("Courier New",Font.BOLD,16));
 		
-		int inc=16;
+		int inc=32;
+		
+		g.drawImage(bottomBox.getImage(), 0, 192, null);
+		g.drawString("Choose a POKeMON.", 32, 16+192+16);
+
 		for (int lp=0; lp<player().party().size(); lp++)
 		{
 			Pokemon pokemon;
 			pokemon = player().party().get(lp);
 		
-			g.drawString( pokemon.nickname(), 48, lp*inc);
-			g.drawString( ":L"+pokemon.getLevel(),208,lp*inc);
-			g.drawString( pokemon.getCurrentHP() + "/" + pokemon.getBaseHP(), 208, (lp*inc)+16);
+			g.drawString( pokemon.nickname(), 48, 16+lp*inc);
+			g.drawString( ":L"+pokemon.getLevel(),208,16+lp*inc);
+			g.drawString( pokemon.getCurrentHP() + "/" + pokemon.getBaseHP(), 208, 32+(lp*inc));
 			
 		}
 		
+		
 		g.drawImage(arrow.getImage(), 0, 16+ pkmnCursorIndex* 32, null);
-
+		
 	}
 	
 	public void step(int ms){}
 	
 	public void buttonPressed(Button b){
-		if (b==Button.START)	enterPresenter(oldPresenter);
+		if (b==Button.START) { enterPresenter(oldPresenter); }
 		else if (b==Button.DOWN){
 			
-			if (pkmnCursorIndex <= player().party().size()) { pkmnCursorIndex++; }
+			if (pkmnCursorIndex < player().party().size()-1) { pkmnCursorIndex++; }
 		
 		}
 		else if (b==Button.UP){
-			if (pkmnCursorIndex >= 0) { pkmnCursorIndex--; }
+			if (pkmnCursorIndex > 0) { pkmnCursorIndex--; }
 			
 		}
 		
