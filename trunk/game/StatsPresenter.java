8f80b7ec9a911df5d44c0957471e4dcbe78a81c6
jjftails <jjftails@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-05-26 15:55:34 +0000

    stats presenter

---
+++ trunk/game/StatsPresenter.java
@@ -0,0 +1,36 @@
+package game;
+
+import java.awt.Graphics2D;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
+
+import java.util.*;
+
+public class StatsPresenter extends Presenter {
+	
+	/**
+	* the presenter to go back to
+	*/
+	Presenter oldPresenter;
+	
+	Pokemon pokemon;
+
+	final ImageIcon statsScreen = new ImageIcon("./resources/stats.png");
+
+	
+	public StatsPresenter(Pokemon p, Presenter oldP){
+		oldPresenter = oldP;	
+		pokemon = p;
+	}
+	
+	public void drawOn(Graphics2D g){
+
+		g.drawImage( pokemon.species().image80(), 16, 16, null);
+		g.drawString(String.valueOf(pokemon.species().number()), 41, 116)
+		
+	}
+	
+}
