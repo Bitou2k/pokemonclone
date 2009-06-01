067d626de7b7f35a9e4a50a80f6c700fcdc0b049
ashendrickson <ashendrickson@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-06-01 02:14:06 +0000

    To make things easier in the end... a talker is a citizen that can only talk. eventually i will have them able to walk ppl need to find images of different talkers walking

--- game/Citizen.java
+++ game/Citizen.java
@@ -6,18 +6,7 @@ package game;
 public class Citizen extends Entity {
 
 	private String name = "UNSETNAME";
-	//private String talk = "HELLO WORLD";
-	//private Image[] image;
 	private Direction d  = Direction.NORTH;
-
-	//Citizen(Tile t, Image[] i, Direction dir, String n, String i)
-	//{
-	//	tile(t);
-	//	d = dir;
-	//	image = i;
-	//	name = n;
-	//	talk = i;
-	//}
 
 	public Direction direction(){ return d; }
 	public void direction(Direction d){	this.d = d;	}
---
+++ game/Talker.java
@@ -0,0 +1,84 @@
+package game;
+import java.util.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+*You.
+*/
+public class Talker extends Citizen
+{
+
+	public Talker(String name, String s, Image[] i, Direction d)
+	{
+		name(name);
+		talk = s;
+		images = i; 
+		direction(d);
+	}
+
+	
+	Image[] images;
+
+	private String talk;
+
+	public String talk()
+	{
+		return talk;
+	}
+
+
+	public void step(int ms)
+	{
+		if (ms % 100 != 0) return;
+
+		stride--;
+		if (stride < 0) stride = 0;
+	}
+
+	int stepCount = 0;
+
+	private int stride = 0; //2, 1, or 0
+	public void startStride() { stride = 2; stepCount++; }
+	/**
+	*Am I in the middle of walking?  If so, don't try to walk again yet.
+	*/
+	public boolean inStride()
+	{
+		return stride > 0;
+	}
+
+	public void drawOn(Graphics2D g)
+	{
+		g.drawImage(getImage(), direction().dx() * -4 * stride, direction().dy() * -4 * stride - 4, null);
+	}
+
+	private Image getImage()
+	{
+		if(images.length == 10)
+		{
+			if (direction() == Direction.NORTH && stride == 0) return images[0];
+			if (direction() == Direction.NORTH && stride > 0)
+				if (stepCount % 2 == 0) return images[1];
+				else return images[2];
+
+			if (direction() == Direction.SOUTH && stride == 0) return images[3];
+			if (direction() == Direction.SOUTH && stride > 0)
+				if (stepCount % 2 == 0) return images[4];
+				else return images[5];
+
+			if (direction() == Direction.EAST && stride == 0) return images[6];
+			if (direction() == Direction.EAST && stride == 1) return images[6];
+			if (direction() == Direction.EAST && stride == 2) return images[7];
+
+			if (direction() == Direction.WEST && stride == 0) return images[8];
+			if (direction() == Direction.WEST && stride == 1) return images[8];
+			if (direction() == Direction.WEST && stride == 2) return images[9];
+		}
+		return null;//error!
+	}
+
+}
