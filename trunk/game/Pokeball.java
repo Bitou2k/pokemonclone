76249a479b165c93efae0f8b09c7413bbf9e7a20
jjftails <jjftails@33af7946-2b5c-11de-ba90-e1c2dc4d389a>
2009-06-01 13:47:21 +0000

    pokeball item class

---
+++ trunk/game/Pokeball.java
@@ -0,0 +1,21 @@
+package game;
+
+import java.util.*;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Pokeball extends Item{
+
+	public Pokeball(Node n)
+	{
+		super(n);
+	}
+	public void use()
+	{
+		//* does different things in wild battle */
+	}
+	
+}
